d3d804ccd569ee4bb35fc11253605444ee940154
Bit Tim <43250787+BitTim@users.noreply.github.com>
2019-01-13 17:44:31 +0100

    Update compile.bat

--- Windows/compile.bat
+++ Windows/compile.bat
@@ -1,5 +1,9 @@
 @echo off
+echo Creating icon
+windres icon.rc -O coff -o icon.res
+echo Icon created!
+echo .
 echo Compiling main.cpp
-g++ main.cpp -IC:\Dev\SDL2-mingw32-32bit\include -LC:\Dev\SDL2-mingw32-32bit\lib -w -Wl,-subsystem,windows -lSDL2main -lSDL2 -lSDL2_ttf -lSDL2_mixer -o main.exe
+g++ main.cpp icon.res -IC:\Dev\SDL2-mingw32-32bit\include -LC:\Dev\SDL2-mingw32-32bit\lib -w -Wl,-subsystem,windows -lSDL2main -lSDL2 -lSDL2_ttf -lSDL2_mixer -o main.exe
 echo Compiled to main.exe!
 pause
